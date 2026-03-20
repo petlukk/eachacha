700b1f346a27a01530ef0be6277fa16689741863
peter lukka <peter.lukka@gmail.com>
2026-03-20 06:49:36 +0000

    build: add chacha20_search.ea to build script

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ echo "Building chacha20_fused.ea..."
 $EA chacha20_fused.ea --lib --opt-level=3
 $EA bind chacha20_fused.ea --python
 
+if [ -f chacha20_search.ea ]; then
+  echo "Building chacha20_search.ea..."
+  $EA chacha20_search.ea --lib --opt-level=3
+  $EA bind chacha20_search.ea --python
+fi
+
 echo "Building chacha20_ref.c..."
 cc -O3 -shared -fPIC -o libchacha20_ref.so chacha20_ref.c
 
